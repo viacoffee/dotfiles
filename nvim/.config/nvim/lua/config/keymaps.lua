2dcdc5e958d3ad7cb67ddf22642b5dee7b556cc0
David Knight <david+github@knight.sh>
2025-12-19 16:00:54 -0500

    dupe

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,6 @@ vim.keymap.set("n", "<leader>s", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>/", ":nohl<CR>")
 -- open Neotree
 vim.keymap.set("n", "<leader>nt", "<Cmd>Neotree toggle reveal<CR>")
--- <leader>/ turn off search highlighting
-vim.keymap.set("n", "<leader>/", ":nohl<CR>")
 -- telescope
 vim.keymap.set("n", "<leader>f", ":Telescope find_files hidden=true<CR>")
 
