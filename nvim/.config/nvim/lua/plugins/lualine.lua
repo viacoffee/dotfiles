473843d7a2a5871fd4c0168feff175cfe704230f
David Knight <david+github@knight.sh>
2025-12-20 11:03:36 -0500

    shorten longer paths

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -10,11 +10,17 @@ return {
                 sections = {
                     lualine_a = {'mode'},
                     lualine_b = {'branch', 'diff', 'diagnostics'},
-                    lualine_c = { {'filename', path = 3,}, },
+                    lualine_c = {
+                        {
+                            'filename',
+                            path = 3,
+                            shorting_target = 40,
+                        },
+                    },
                     lualine_x = {'filetype'},
                     lualine_y = {'progress'},
                     lualine_z = {'location'}
-                }
+                },
             })
         end
     }
