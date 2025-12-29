3f8aa17823a9f2f75d5fb01e56ab258a6a71effa
David Knight <david+github@knight.sh>
2025-12-29 10:09:12 -0500

    lualine changes

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -1,31 +1,4 @@
 return {
-    --{
-    --    "xiyaowong/transparent.nvim",
-    --    lazy = false,
-    --    config = function()
-    --        require("transparent").setup({
-    --            enable = true,
-    --            -- table: default groups
-    --            groups = {
-    --                'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-    --                'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-    --                'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-    --                'SignColumn', 'StatusLine', 'StatusLineNC', 'EndOfBuffer',
-    --            },
-    --            -- table: additional groups that should be cleared
-    --            extra_groups = {},
-    --            -- table: groups you don't want to clear
-    --            extra_groups = {
-    --                "NvimTreeNormal",
-    --                "NeoTreeNormal",
-    --                "NeoTreeNormalNC",
-    --            },
-    --            -- function: code to be executed after highlight groups are cleared
-    --            -- Also the user event "TransparentClear" will be triggered
-    --            on_clear = function() end,
-    --        })
-    --    end,
-    --},
     {
         "nvimdev/indentmini.nvim",
         config = function()
@@ -35,14 +8,35 @@ return {
     {
         "Darazaki/indent-o-matic",
         config = function()
-            require('indent-o-matic').setup({
-                -- The values indicated here are the defaults
-                -- Number of lines without indentation before giving up (use -1 for infinite)
+            require("indent-o-matic").setup({
                 max_lines = 2048,
-                -- Space indentations that should be detected
                 standard_widths = { 2, 4, 8 },
-                -- Skip multi-line comments and strings (more accurate detection but less performant)
                 skip_multiline = true,
+            })
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "nordic",
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "filetype" },
+                    lualine_c = {
+                        {
+                            "filename",
+                            path = 3,
+                            shortening_target = 40,
+                        },
+                    },
+                    lualine_x = { "diagnostics" },
+                    lualine_y = { "diff" },
+                    lualine_z = { "branch" },
+                },
             })
         end,
     },
