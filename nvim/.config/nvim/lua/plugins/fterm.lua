57253dc9afd18c8f9f623c6c0c6c0df7a6ed042e
David Knight <david+github@knight.sh>
2026-01-04 10:42:25 -0500

    add lazygit to fterm popup

--- nvim/.config/nvim/lua/plugins/fterm.lua
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -1,12 +1,28 @@
 return {
-    "numToStr/FTerm.nvim",
-    config = function()
-        require("FTerm").setup({
-            border = "rounded",
-            dimensions = {
-                height = 0.8,
-                width = 0.8,
-            },
-        })
-    end,
+    {
+        "numToStr/FTerm.nvim",
+        config = function()
+            local fterm = require("FTerm")
+            fterm.setup({
+                border = "rounded",
+                dimensions = {
+                    height = 0.8,
+                    width = 0.8,
+                },
+            })
+        end,
+    },
+    {
+        'numToStr/FTerm.nvim',
+        name = 'FTerm.LazyGit.nvim',
+        config = function()
+            local fterm = require("FTerm")
+            local lazygit = fterm:new({
+                cmd = 'lazygit',
+            })
+
+            vim.keymap.set('n', '<A-g>', function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
+            vim.keymap.set('t', '<A-g>', function() lazygit:toggle() end, {desc = 'Toggle [L]azy[G]it'})
+        end,
+    }
 }
