ffb2e3aa11a0a96dc1897d7ab8b248cd8c930533
David Knight <231013+viacoffee@users.noreply.github.com>
2026-02-06 19:02:01 -0500

    mimetypes, thunar, and xdg-utils

--- install/80-defaults.sh
+++ install/80-defaults.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Default terminal
-echo "Set default terminal to alacritty"
-xdg-mime default alacritty.desktop x-scheme-handler/terminal || true
-
-# Default browser
-echo "Set default browser to firefox"
-xdg-settings set default-web-browser firefox.desktop || true
-
 echo "Creating default home directories"
 DEFAULT_DIRS=(
   Notes
