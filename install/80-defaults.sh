b506aeb53d21f85cf531c6b18a7e19b901d949a6
David Knight <231013+viacoffee@users.noreply.github.com>
2026-02-06 19:17:09 -0500

    force gtk applications to dark-mode

--- install/80-defaults.sh
+++ install/80-defaults.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Try for force dark mode on gtk applications
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+# Create base home directories
 echo "Creating default home directories"
 DEFAULT_DIRS=(
   Notes
