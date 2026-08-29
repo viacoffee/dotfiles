b0d3d98e1b5c2a284d86286dfdfcdbb4ce2db841
David Knight <231013+viacoffee@users.noreply.github.com>
2026-08-29 13:06:15 -0400

    remove the notification script

--- config/waybar/indicators/notification.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-count=$(makoctl list 2>/dev/null | grep -c '^Notification ') 
-
-if [ "$count" -gt 0 ]; then
-  echo '{"text": "󰅾", "tooltip": "View notifications", "class": "active"}'
-else
-  echo '{"text": ""}'
-fi
